bef06209b21e0bad3e0891118c21c124e7c87aad
arpadav <arpadav@gmail.com>
2024-09-12 21:52:01 -0400

    more examples, once encode work to test

--- src/codecs/mod.rs
+++ src/codecs/mod.rs
@@ -1,3 +1,6 @@
+use num_traits::ToBytes;
+use std::convert::AsRef;
+
 pub mod ber;
 pub mod binary;
 pub mod string;
@@ -19,4 +22,23 @@ re_export! {
     ber,
     string,
     binary
+}
+
+pub struct FixedLength {}
+impl FixedLength {
+    pub fn decode<P>(len: usize) -> impl Fn(&mut &[u8]) -> winnow::PResult<P>
+    where
+        P: From<u128>,
+    {
+        move |input: &mut &[u8]| {
+            let val = crate::codecs::binary::dec::be_u128_lengthed(input, len)?;
+            Ok(val.into())
+        }
+    }
+    pub fn encode<P>(len: usize) -> impl Fn(&P) -> Vec<u8>
+    where
+        P: ToBytes
+    {
+        move |input: &P| input.to_be_bytes().as_ref()[..len].to_vec()
+    }
 }
